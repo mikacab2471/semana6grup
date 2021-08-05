d4b6894ac4107f1edf9ba973e192afb98c7e54cd
mikacab2471 <88199791+mikacab2471@users.noreply.github.com>
2021-08-05 10:12:40 -0300

    1

---
+++ listadojava.js
@@ -0,0 +1,27 @@
+const listado_URL = "https://tomasmarquez99.github.io/tabla-apertura/listado.json"
+
+
+function cargarDatos(URL){
+    document.getElementById("datostabla").innerHTML = "";
+    fetch(URL)
+        .then(respuesta => respuesta.json())
+        .then(datos => {
+               
+                for (valor of datos){
+                    document.getElementById("datostabla").innerHTML += `
+                    <tr>
+                <td>
+                    ${valor.atributo1}
+                </td>
+                <td>
+                     ${valor.atributo2}
+                </td>
+            
+            </tr>
+                    
+                    ` 
+                        
+                }
+        })
+        .catch(error => alert("Hubo un error: " + error));
+}
